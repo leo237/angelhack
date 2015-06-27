a44b2c08a0eaed89e9407688955cc36642e141cc
leo237 <abhilashpanigrahi@hotmail.com>
2015-06-27 23:42:16 +0530

    Login Handlers for all three types of people

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ import json
 import requests
 import random
 import tornado.escape
+import pycps
 from hashlib import sha512
+import urllib2, urllib, json
 #---------------------------------------------------------------------------
 
 from tornado.options import define, options, parse_command_line
@@ -19,18 +21,94 @@ from tornado.options import define, options, parse_command_line
 
 #---------------------------------------------------------------------------
 
-# from pymongo import MongoClient
-# client = MongoClient()
-# db = client['angelhack']
+#clusterpoint APIS
+
+# head = {
+#     "Authorization":"Basic bGVvcGFuaWdyYWhpQGdtYWlsLmNvbToyM2xlbzIz"
+# }
+# url = "https://api-us.clusterpoint.com/100629/zeno/_search.json"
+# values = dict(query='abhilash')
+# r = requests.post(url,data=json.dumps(values), headers=headers)
+# print r.content
+
 
 #-------------------------------------------------------
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         if (self.get_secure_cookie('user')):
-            self.redirect('/check')
+            self.redirect('/home')
         else:
             self.render('index.html')
-        
+
+#Login Handlers----------------------------------------
+class CollegeLoginHandler(tornado.web.RequestHandler):
+    def post(self):
+        username = re.escape(self.get_argument("u"))
+        rawPassword = re.escape(self.get_argument("p"))
+        #Processing password
+        m = hashlib.md5()
+        m.update(rawPassword)
+        password = m.hexdigest()
+        #We have username and password. Now verify using clusterpoint
+        url = "https://api-us.clusterpoint.com/100629/college/_search.json"
+        headers = {"Authorization":"Basic bGVvcGFuaWdyYWhpQGdtYWlsLmNvbToyM2xlbzIz"}
+        string = "<u>"+username+"</u><p>"+password+"</p>"
+        values = dict(query=string)
+        r = requests.post(url,data=json.dumps(values), headers=headers)
+        responseDict = json.loads(r.content)
+        if responseDict['documents'] is not None:
+            collegeID = responseDict['documents'][0]['u']
+            self.set_secure_cookie('college', collegeID)
+            self.redirect('/collegehome')
+        else:
+            self.redirect('/')
+
+class StudentLoginHandler(tornado.web.RequestHandler):
+    def post(self):
+        username = re.escape(self.get_argument("u"))
+        rawPassword = re.escape(self.get_argument("p"))
+        #Processing password
+        m = hashlib.md5()
+        m.update(rawPassword)
+        password = m.hexdigest()
+        #We have username and password. Now verify using clusterpoint
+        url = "https://api-us.clusterpoint.com/100629/student/_search.json"
+        headers = {"Authorization":"Basic bGVvcGFuaWdyYWhpQGdtYWlsLmNvbToyM2xlbzIz"}
+        string = "<regno>"+username+"</regno><pwd>"+password+"</pwd>"
+        values = dict(query=string)
+        r = requests.post(url,data=json.dumps(values), headers=headers)
+        responseDict = json.loads(r.content)
+        if responseDict['documents'] is not None:
+            studentID = responseDict['documents'][0]['regno']
+            self.set_secure_cookie('student', studentID)
+            self.redirect('/studenthome')
+        else:
+            self.redirect('/')
+
+class CompanyLoginHandler(tornado.web.RequestHandler):
+    def post(self):
+        username = re.escape(self.get_argument("u"))
+        rawPassword = re.escape(self.get_argument("p"))
+        #Processing password
+        m = hashlib.md5()
+        m.update(rawPassword)
+        password = m.hexdigest()
+        #We have username and password. Now verify using clusterpoint
+        url = "https://api-us.clusterpoint.com/100629/company/_search.json"
+        headers = {"Authorization":"Basic bGVvcGFuaWdyYWhpQGdtYWlsLmNvbToyM2xlbzIz"}
+        string = "<id>"+username+"</id><pwd>"+password+"</pwd>"
+        values = dict(query=string)
+        r = requests.post(url,data=json.dumps(values), headers=headers)
+        responseDict = json.loads(r.content)
+        if responseDict['documents'] is not None:
+            companyID = responseDict['documents'][0]['id']
+            self.set_secure_cookie('company', companyID)
+            self.redirect('/companyhome')
+        else:
+            self.redirect('/')
+
+
+
             
 
 class LogoutHandler(tornado.web.RequestHandler):
@@ -62,6 +140,8 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         
 handlers = [
     (r'/',IndexHandler),
+    (r'/collegelogin',CollegeLoginHandler),
+
     (r'/logout',LogoutHandler)
 
 ]
